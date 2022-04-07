89a714d00e63be0c0255cd492f08616492225de8
Seb Insua <me@sebinsua.com>
2022-04-07 14:12:26 +0100

    Tighten

---
+++ src/createRoutes.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+import { sql } from 'slonik';
+
+import { createTypedRoutes } from './core';
+import { connection } from './database';
+
+export function createRoutes() {
+  return createTypedRoutes((app) => {
+    app.route({
+      method: 'GET',
+      url: '/',
+      schema: {
+        operationId: 'getRoot',
+        summary: 'Get root',
+        description: 'Longer description goes here.',
+        tags: ['root'],
+        querystring: z.object({
+          name: z.string(),
+        }),
+        response: {
+          200: z.object({
+            message: z.string(),
+            rows: z.array(z.any()),
+          }),
+        },
+      },
+      async handler(request, reply) {
+        request.log.info('foo');
+
+        const results = await connection.query(sql`SELECT * FROM foo`);
+
+        return reply.send({
+          message: `Hello ${request.query.name}`,
+          rows: [...results.rows],
+        });
+      },
+    });
+  });
+}
--- src/startServer.ts
+++ src/startServer.ts
@@ -1,9 +1,10 @@
 import { serializeError } from 'serialize-error';
 
-import { createServer, serverLog } from './core/createServer';
-import routes from './routes';
+import { createServer, serverLog } from './core';
+import { createRoutes } from './createRoutes';
 
-const app = createServer(routes);
+const mainRoutes = createRoutes();
+const app = createServer(mainRoutes);
 
 app.listen({ port: 3000, host: '0.0.0.0' }, (error) => {
   if (error) {
