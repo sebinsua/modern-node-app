dd036f7e72e62bc7ba8c5aa40885a7904c957978
Seb Insua <me@sebinsua.com>
2022-09-13 21:42:08 +0100

    Use runtime type validation for database rows

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 import { createTypedRoutesPlugin } from '../core';
-import { getPgTables } from './getRoot';
+import { getPgTables, PgTable } from './getRoot';
 
 export default createTypedRoutesPlugin((app, _, done) => {
   app.route({
@@ -18,18 +18,7 @@ export default createTypedRoutesPlugin((app, _, done) => {
       response: {
         200: z.object({
           message: z.string(),
-          rows: z.array(
-            z.object({
-              schemaname: z.string().nullable(),
-              tablename: z.string().nullable(),
-              tableowner: z.string().nullable(),
-              tablespace: z.string().nullable(),
-              hasindexes: z.boolean().nullable(),
-              hasrules: z.boolean().nullable(),
-              hastriggers: z.boolean().nullable(),
-              rowsecurity: z.boolean().nullable(),
-            })
-          ),
+          rows: z.array(PgTable),
         }),
       },
     },
