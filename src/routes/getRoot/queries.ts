5ba38855ba273feb5105d369baf1ff262e1ceb4d
Seb Insua <me@sebinsua.com>
2023-01-27 17:16:57 +0000

    Fix types

--- src/routes/getRoot/queries.ts
+++ src/routes/getRoot/queries.ts
@@ -8,10 +8,10 @@ export const PgTable = z.object({
   tablename: z.string().nullable(),
   tableowner: z.string().nullable(),
   tablespace: z.string().nullable(),
-  hasindexes: z.boolean().nullable(),
-  hasrules: z.boolean().nullable(),
-  hastriggers: z.boolean().nullable(),
-  rowsecurity: z.boolean().nullable(),
+  hasindexes: z.boolean(),
+  hasrules: z.boolean(),
+  hastriggers: z.boolean(),
+  rowsecurity: z.boolean(),
 });
 
 export async function getPgTables() {
